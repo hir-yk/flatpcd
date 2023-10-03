7c9a702d9ea85ed8a739d09146c6dbcf7f0a960c
hir-yk <84175137+hir-yk@users.noreply.github.com>
2023-10-03 16:12:32 +0900

    Command line argument customization

--- createflatfield.py
+++ createflatfield.py
@@ -1,8 +1,21 @@
+import argparse
+
+## コマンドライン引数の定義
+parser = argparse.ArgumentParser()
+parser.add_argument('--origin_x', type=int, default=0, help='x coordinate of the origin in meters (default: 0)')
+parser.add_argument('--origin_y', type=int, default=0, help='y coordinate of the origin in meters (default: 0)')
+parser.add_argument('--range_x', type=int, default=100, help='Range x in meters (default: 100)')
+parser.add_argument('--range_y', type=int, default=100, help='Range y in meters (default: 100)')
+parser.add_argument('--interval', type=float, default=1, help='Interval between points in meters (default: 1)')
+args = parser.parse_args()
+
 ## settings
-OUTFILE='testfile.pcd'
-range_x=100000  #[mm]
-range_y=100000
-interval=500
+OUTFILE = 'testfile.pcd'
+origin_x = args.origin_x * 1000 # Convert to millimeters
+origin_y = args.origin_y * 1000
+range_x = args.range_x * 1000
+range_y = args.range_y * 1000
+interval = args.interval * 1000
 
 n_points = (range_x/interval)*(range_y/interval)
 n_points_str = str(int(n_points)) 
@@ -28,13 +41,14 @@ DATA ascii
 head=head_1+n_points_str+head_2+n_points_str+head_3
 
 
-## create newpcd ##
-f=open(OUTFILE, 'w')
+f = open(OUTFILE, 'w')
 f.write(head)
 
-for x in range(0, range_x, interval):
-  for y in range(0, range_y, interval):
-    print(x/1000, y/1000, 0, 20)
-    f.write(' '.join(map(str,[x/1000, y/1000, 0, 20]))+'\n' )
+# 使用するrange関数を整数用から浮動小数点数用のnumpy.arangeへ変更し、ステップ数をintervalに設定
+import numpy as np
+for x in np.arange(origin_x, origin_x + range_x, interval):
+    for y in np.arange(origin_y, origin_y + range_y, interval):
+        print(x / 1000, y / 1000, 0, 20)
+        f.write(' '.join(map(str, [x / 1000, y / 1000, 0, 20])) + '\n')
 
 f.close()
